085aec4ff1f013979554cbe268348c50d6345839
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2021-09-26 23:25:17 +0900

    change index.php

--- work/public/index.php
+++ work/public/index.php
@@ -9,12 +9,47 @@ try {
     DSN,
     DB_USER,
     DB_PASS,
-    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,]
+    [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, PDO::ATTR_EMULATE_PREPARES => false,
+    ]
   );
 } catch (PDOException $e) {
   echo $e->getMessage();
   exit;
 }
+
+function h($str)
+{
+  return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+
+function addTodo($pdo)
+{
+  $title = trim(filter_input(INPUT_POST, 'title'));
+  if ($title === '') {
+    return;
+  }
+
+  $stmt = $pdo->prepare("INSERT INTO todos (title) VALUES (:title)");
+  $stmt->bindValue('title', $title, PDO::PARAM_STR);
+  $stmt->execute();
+}
+
+function getTodos($pdo)
+{
+  $stmt = $pdo->query("SELECT * FROM todos ORDER BY id DESC");
+  $todos = $stmt->fetchAll();
+  return $todos;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  addTodo($pdo);
+}
+
+$todos = getTodos($pdo);
+// var_dump($todos);
+// exit;
 
 ?>
 
@@ -31,19 +66,19 @@ try {
 <body>
   <h1>Todos</h1>
 
+  <form action="" method="post">
+    <input type="text" name="title" placeholder="Type new todo.">
+  </form>
+
   <ul>
-    <li>
-      <input type="checkbox">
-      <span>Title</span>
-    </li>
-    <li>
-      <input type="checkbox" checked>
-      <span class="done">Title</span>
-    </li>
-    <li>
-      <input type="checkbox">
-      <span>Title</span>
-    </li>
+    <?php foreach ($todos as $todo) : ?>
+      <li>
+        <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
+        <span class="<?= $todo->is_done ? 'done' : ''; ?>">
+          <?= h($todo->title); ?>
+        </span>
+      </li>
+    <?php endforeach; ?>
   </ul>
 </body>
 
