9e836af50a749013fcb79ffb04c00bd7400dc26c
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2021-09-26 22:59:44 +0900

    create todolist

---
+++ work/public/index.php
@@ -0,0 +1,50 @@
+<?php
+
+define('DSN', 'mysql:host=db;dbname=myapp;charset=utf8mb4');
+define('DB_USER', 'myappuser');
+define('DB_PASS', 'myapppass');
+
+try {
+  $pdo = new PDO(
+    DSN,
+    DB_USER,
+    DB_PASS,
+    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,]
+  );
+} catch (PDOException $e) {
+  echo $e->getMessage();
+  exit;
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <title>My Todos</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+  <h1>Todos</h1>
+
+  <ul>
+    <li>
+      <input type="checkbox">
+      <span>Title</span>
+    </li>
+    <li>
+      <input type="checkbox" checked>
+      <span class="done">Title</span>
+    </li>
+    <li>
+      <input type="checkbox">
+      <span>Title</span>
+    </li>
+  </ul>
+</body>
+
+</html>
