29e760dff5e0b7d33c0a179d0666f18ad0bbc40e
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2021-09-30 13:06:46 +0900

    add purge

--- work/app/config.php
+++ work/app/config.php
@@ -5,7 +5,21 @@ session_start();
 define('DSN', 'mysql:host=db;dbname=myapp;charset=utf8mb4');
 define('DB_USER', 'myappuser');
 define('DB_PASS', 'myapppass');
-// define('SITE_URL','http://localhost:8562');
 define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
 
-require_once(__DIR__ . '/functions.php');
+spl_autoload_register(function ($class) {
+    $prefix = 'MyApp\\';
+
+    if (strpos($class, $prefix) === 0) {
+        // MyApp\Database
+        // $fileName = sprintf(__DIR__ . '/%s.php', substr($class, 6));
+        $fileName = sprintf(__DIR__ . '/%s.php', substr($class, strlen($prefix)));
+
+        if (file_exists($fileName)) {
+            require($fileName);
+        } else {
+            echo 'File not found: ' . $fileName;
+            exit;
+        }
+    }
+});
--- work/public/index.php
+++ work/public/index.php
@@ -2,41 +2,17 @@
 
 require_once(__DIR__ . '/../app/config.php');
 
-createToken();
+use MyApp\Database;
+use MyApp\Todo;
+use MyApp\Utils;
 
-$pdo = getPdoInstance();
+$pdo = Database::getInstance();
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  validateToken();
-  $action = filter_input(INPUT_GET, 'action');
-
-  switch ($action) {
-    case 'add':
-      addTodo($pdo);
-      break;
-    case 'toggle':
-      toggleTodo($pdo);
-      break;
-    case 'delete':
-      deleteTodo($pdo);
-      break;
-    default:
-      exit;
-  }
-
-  addTodo($pdo);
-
-  header('Location: ' . SITE_URL);
-  exit;
-}
-
-$todos = getTodos($pdo);
-// var_dump($todos);
-// exit;
+$todo = new Todo($pdo);
+$todo->processPost();
+$todos = $todo->getAll();
 
 ?>
-
-
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -48,11 +24,17 @@ $todos = getTodos($pdo);
 
 <body>
   <main>
-    <h1>Todos</h1>
+    <header>
+      <h1>Todos</h1>
+      <form action="?action=purge" method="post">
+        <span class="purge">Purge</span>
+        <input type="hidden" name="token" value="<?= Utils::h($_SESSION['token']); ?>">
+      </form>
+    </header>
 
     <form action="?action=add" method="post">
       <input type="text" name="title" placeholder="Type new todo.">
-      <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
+      <input type="hidden" name="token" value="<?= Utils::h($_SESSION['token']); ?>">
     </form>
 
     <ul>
@@ -60,23 +42,25 @@ $todos = getTodos($pdo);
         <li>
           <form action="?action=toggle" method="post">
             <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
-            <input type="hidden" name="id" value="<?= h($todo->id); ?>">
-            <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
+            <input type="hidden" name="id" value="<?= Utils::h($todo->id); ?>">
+            <input type="hidden" name="token" value="<?= Utils::h($_SESSION['token']); ?>">
           </form>
+
           <span class="<?= $todo->is_done ? 'done' : ''; ?>">
-            <?= h($todo->title); ?>
+            <?= Utils::h($todo->title); ?>
           </span>
 
           <form action="?action=delete" method="post" class="delete-form">
             <span class="delete">x</span>
-            <input type="hidden" name="id" value="<?= h($todo->id); ?>">
-            <input type="hidden" name="token" value="<?= h($_SESSION['token']);  ?>">
+            <input type="hidden" name="id" value="<?= Utils::h($todo->id); ?>">
+            <input type="hidden" name="token" value="<?= Utils::h($_SESSION['token']); ?>">
           </form>
         </li>
       <?php endforeach; ?>
     </ul>
-    <script src="js/main.js"></script>
   </main>
+
+  <script src="js/main.js"></script>
 </body>
 
 </html>
